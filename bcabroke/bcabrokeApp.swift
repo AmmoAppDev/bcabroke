1f4d73930cc87bb2c6385de7e4c1e288d9af6231
Justin Ammons <Justinammons77@gmail.com>
2023-05-05 12:00:10 -0500

    Initial Commit

---
+++ bcabroke/bcabrokeApp.swift
@@ -0,0 +1,17 @@
+//
+//  bcabrokeApp.swift
+//  bcabroke
+//
+//  Created by Justin Ammons on 5/5/23.
+//
+
+import SwiftUI
+
+@main
+struct bcabrokeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
